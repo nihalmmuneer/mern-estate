bdb96380781bd3aaf8d247407bd5753b936fb34c
nihal <nihalmmuneer@gmail.com>
2024-12-09 17:19:09 +0530

    spinner effect

--- client/src/components/FilteredListItem.jsx
+++ client/src/components/FilteredListItem.jsx
@@ -6,47 +6,51 @@ const FilteredListItem = ({ filterData }) => {
   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="flex">
-      <div className=" rounded-lg  my-4 w-full sm:w-[300px]  bg-white shadow-md hover:shadow-lg overflow-hidden flex flex-col gap-2">
-        <Link to={ currentUser ? `/listing/${filterData?._id}`:`/sign-in`}>
-          <img
-            src={filterData?.imageUrls[0]}
-            alt={filterData?.name}
-            className="w-full hover:scale-105 h-[200px] transition-scale transition-transform"
-          />
-        </Link>
-        <div className="p-2 flex flex-col gap-2">
-          <div className="truncate text-slate-800 font-bold">
-            {filterData?.name}
-          </div>
-          <div className="flex items-center gap-1">
-            <MdLocationPin className="text-green-500" />
-            <p className="text-slate-700 text-sm truncate">
-              {filterData?.address}
-            </p>
-          </div>
-          <div className="text-slate-700 line-clamp-2 text-sm">
-            {filterData?.description}
-          </div>
-          <div className="text-slate-500 font-semibold">
-            {filterData?.offer
-              ? `$${filterData?.discountPrice?.toLocaleString("en-US")}`
-              : `$${filterData?.regularPrice?.toLocaleString("en-US")}`}
-            {filterData?.type === "rent" ? "/month" : ""}
-          </div>
-          <div className="flex items-center gap-3 text-slate-700 font-medium text-sm">
-            <div>
-              {filterData?.bedrooms?.length > 1
-                ? `${filterData?.bedrooms} beds`
-                : `${filterData?.bedrooms} bed`}
+      {filterData ? (
+        <img src="/rolling.gif" alt="rolling-gif" />
+      ) : (
+        <div className=" rounded-lg  my-4 w-full sm:w-[300px]  bg-white shadow-md hover:shadow-lg overflow-hidden flex flex-col gap-2">
+          <Link to={currentUser ? `/listing/${filterData?._id}` : `/sign-in`}>
+            <img
+              src={filterData?.imageUrls[0]}
+              alt={filterData?.name}
+              className="w-full hover:scale-105 h-[200px] transition-scale transition-transform"
+            />
+          </Link>
+          <div className="p-2 flex flex-col gap-2">
+            <div className="truncate text-slate-800 font-bold">
+              {filterData?.name}
             </div>
-            <div>
-              {filterData?.bathrooms?.length > 1
-                ? `${filterData?.bathrooms} baths`
-                : `${filterData?.bathrooms} bath`}
+            <div className="flex items-center gap-1">
+              <MdLocationPin className="text-green-500" />
+              <p className="text-slate-700 text-sm truncate">
+                {filterData?.address}
+              </p>
+            </div>
+            <div className="text-slate-700 line-clamp-2 text-sm">
+              {filterData?.description}
+            </div>
+            <div className="text-slate-500 font-semibold">
+              {filterData?.offer
+                ? `$${filterData?.discountPrice?.toLocaleString("en-US")}`
+                : `$${filterData?.regularPrice?.toLocaleString("en-US")}`}
+              {filterData?.type === "rent" ? "/month" : ""}
+            </div>
+            <div className="flex items-center gap-3 text-slate-700 font-medium text-sm">
+              <div>
+                {filterData?.bedrooms?.length > 1
+                  ? `${filterData?.bedrooms} beds`
+                  : `${filterData?.bedrooms} bed`}
+              </div>
+              <div>
+                {filterData?.bathrooms?.length > 1
+                  ? `${filterData?.bathrooms} baths`
+                  : `${filterData?.bathrooms} bath`}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
